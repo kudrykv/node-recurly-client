857755d4c69f14e58ef6b62a864d40a22fca310a
kudrykv <kudryk.v+develop@gmail.com>
2017-01-09 16:32:13 +0200

    remove not useful test

--- test/integration/bootstrap.test.js
+++
@@ -1,8 +0,0 @@
-var assert = require('assert');
-
-describe('Dummy test', function () {
-  it('should check whether needed env variables exist', function testEnvNames () {
-    assert.notEqual(process.env.API_KEY, '', 'API_KEY not set');
-    assert.notEqual(process.env.SUBDOMAIN, '', 'SUBDOMAIN not set');
-  });
-});
